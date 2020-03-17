7b13f5521af98a8b3522d118e81bc00a2f9a8ae9
opar <xuyaguang0630@foxmail.com>
2020-03-17 21:34:40 +0800

    ci(next-config): add buildId

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const git = require('git-rev-sync');
+
+module.exports = {
+  generateBuildId: () => {
+    const buildId = `${git.branch()}-${git.short()}`
+    console.log(`buildId: ${buildId}`)
+    return buildId
+  },
+}
