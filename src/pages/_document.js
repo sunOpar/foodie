dde2165eef3ddf33b9a6d1dd55241db4564ae175
opar <xuyaguang0630@foxmail.com>
2020-04-08 10:15:56 +0800

    fix(_document): fix getInitialProps error

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,27 @@
-import {Head, Main, NextScript} from 'next/document'
+import Document,{ Head, Main, NextScript } from 'next/document'
 
-const StandardDocument = () => (
-  <html>
-    <Head>
-      <meta
-        name="viewport"
-        content="user-scalable=no,width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"
-      />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </html>
-)
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta
+            name="viewport"
+            content="user-scalable=no,width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
 
-export default StandardDocument
+export default MyDocument
