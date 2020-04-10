5ee4aefe746a12efbd44092862891c69c9564d6c
opar <xuyaguang0630@foxmail.com>
2020-04-10 14:06:46 +0800

    refactor: remove git-rev-sync temporary

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const git = require('git-rev-sync');
+// const git = require('git-rev-sync');
 
 module.exports = {
-  generateBuildId: () => {
-    const buildId = `${git.branch()}-${git.short()}`
-    console.log(`buildId: ${buildId}`)
-    return buildId
-  },
+//   generateBuildId: () => {
+//     const buildId = `${git.branch()}-${git.short()}`
+//     console.log(`buildId: ${buildId}`)
+//     return buildId
+//   },
 }
