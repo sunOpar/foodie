1a2e95bbc1d3c3ed380b1d7b9697a68fe8d84f38
opar <xuyaguang0630@foxmail.com>
2020-04-19 10:41:43 +0800

    feat: init edit

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,12 +1,22 @@
+import styled from 'styled-components'
 import Header from './Header'
 import HomeFooter from './HomeFooter'
-import { Box } from 'grommet'
+import MyEditor from './MyEditor'
+
+const EditorWrap = styled.div`
+  width: 708px;
+  margin: 24px auto;
+  font-size: 16px;
+  line-height: 1.4;
+`
 
 const HomeView = () => {
   return (
     <div>
       <Header />
-      <Box height="65vh" />
+      <EditorWrap>
+        <MyEditor />
+      </EditorWrap>
       <HomeFooter />
     </div>
   )
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,4 @@
 export const theme = {
   global: {
-    font: {
-      family: 'Roboto',
-      size: '14px',
-      height: '20px',
-    },
   },
 }
